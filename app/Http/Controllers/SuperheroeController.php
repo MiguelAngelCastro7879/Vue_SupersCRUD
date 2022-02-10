b0e108dc78e53ccdd905988d75422ee38ca58088
MiguelAngelCastro7879 <mikeangel7879@gmail.com>
2022-02-09 19:35:43 -0600

    Se crea el factory, la migracion y el controlador del superheroe

---
+++ app/Http/Controllers/SuperheroeController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Superheroe;
+use Illuminate\Http\Request;
+
+class SuperheroeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Superheroe  $superheroe
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Superheroe $superheroe)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Superheroe  $superheroe
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Superheroe $superheroe)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Superheroe  $superheroe
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Superheroe $superheroe)
+    {
+        //
+    }
+}
